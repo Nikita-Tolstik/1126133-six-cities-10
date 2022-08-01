6aef33ac8419229572f2284a0a41faba83407fac
Nikita Tolstik <tolstik-nik@mail.ru>
2022-08-01 21:05:36 +0300

    Добавляет компоненты Header, HeaderUser, HeaderGuest. Подключает Header в компонент MainScreen

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { PageCardClass } from '../../const';
-import Logo from '../../components/logo/logo';
-import CitiesTabs from '../../components/cities-tabs/cities-tabs';
 import { useAppSelector } from '../../hooks';
 import { getActiveCityOffers } from '../../utils/utils';
+import Header from '../../components/header/header';
+import CitiesTabs from '../../components/cities-tabs/cities-tabs';
 import MainOffers from '../../components/main-offers/main-offers';
 import MainOffersEmpty from '../../components/main-offers-empty/main-offers-empty';
 
@@ -19,32 +19,8 @@ const MainScreen: React.FC = () => {
 
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Logo />
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="/">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="/">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+
+      <Header />
 
       <main
         className={`page__main page__main--index ${isEmptyOffers ? 'page__main--index-empty' : ''}`}

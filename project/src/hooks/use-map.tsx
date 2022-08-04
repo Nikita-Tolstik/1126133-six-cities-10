2559fefeec26d315ec8105ee48abf02661b9df87
Nikita Tolstik <tolstik-nik@mail.ru>
2022-08-04 18:35:11 +0300

    Переименовывает названия файлов в соответствии с критериями

---
+++ project/src/hooks/use-map.tsx
@@ -0,0 +1,40 @@
+import { Map, TileLayer } from 'leaflet';
+import { MutableRefObject, useEffect, useState, useRef } from 'react';
+import { Location } from '../types/offers';
+
+const useMap = (
+  mapRef: MutableRefObject<HTMLElement | null>,
+  locationCity: Location
+): Map | null => {
+
+  const [map, setMap] = useState<Map | null>(null);
+  const isRenderedRef = useRef<boolean>(false);
+
+  useEffect(() => {
+    if (mapRef.current !== null && !isRenderedRef.current) {
+      const instance = new Map(mapRef.current, {
+        center: {
+          lat: locationCity.latitude,
+          lng: locationCity.longitude
+        },
+        zoom: locationCity.zoom
+      });
+
+      const layer = new TileLayer(
+        'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+        {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        }
+      );
+
+      instance.addLayer(layer);
+      setMap(instance);
+      isRenderedRef.current = true;
+    }
+  }, [mapRef, locationCity]);
+
+  return map;
+};
+
+export default useMap;

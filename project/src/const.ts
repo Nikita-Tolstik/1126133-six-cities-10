ef22a5652112cf3ff5519956eb04b18114e90d6c
Nikita Tolstik <tolstik-nik@mail.ru>
2022-08-04 18:36:52 +0300

    Добавляет изображение активного маркера

--- project/src/const.ts
+++ project/src/const.ts
@@ -83,7 +83,7 @@ export const inputRatingOption = [
 
 export const UrlMarker = {
   Default: 'img/pin.svg',
-  Current: 'img/pin-active.svg'
+  Active: 'img/pin-active.svg'
 } as const;
 
 export const IconParameter = {
@@ -117,6 +117,12 @@ export const city = [
 
 export const defaultCustomIcon = new Icon({
   iconUrl: UrlMarker.Default,
+  iconSize: [IconParameter.Size.x, IconParameter.Size.y],
+  iconAnchor: [IconParameter.Anchor.x, IconParameter.Anchor.y],
+});
+
+export const activeCustomIcon = new Icon({
+  iconUrl: UrlMarker.Active,
   iconSize: [IconParameter.Size.x, IconParameter.Size.y],
   iconAnchor: [IconParameter.Anchor.x, IconParameter.Anchor.y],
 });

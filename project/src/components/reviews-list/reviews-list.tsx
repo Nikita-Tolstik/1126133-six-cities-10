646f21211dbc40986d8b470521c5d1277e4ef5cc
Nikita Tolstik <tolstik-nik@mail.ru>
2022-08-09 01:34:14 +0300

    Дорабатывает ReviewsList: не больше 10 отзывов; отсортированы от новых к старым

--- project/src/components/reviews-list/reviews-list.tsx
+++ project/src/components/reviews-list/reviews-list.tsx
@@ -6,15 +6,22 @@ type ReviewsListProps = {
   reviews: Reviews;
 }
 
-const ReviewsList: React.FC<ReviewsListProps> = ({ reviews }) => (
-  <ul className="reviews__list">
-    {reviews.map((review) => (
-      <UserReview
-        key={review.id}
-        review={review}
-      />)
-    )}
-  </ul>
-);
+const MAX_COMMENT = 10;
+
+const ReviewsList: React.FC<ReviewsListProps> = ({ reviews }) => {
+  const orderedReviews = reviews.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
+  const limitReviews = orderedReviews.length > MAX_COMMENT ? orderedReviews.slice(0, MAX_COMMENT) : orderedReviews;
+
+  return (
+    <ul className="reviews__list">
+      {limitReviews.map((review) => (
+        <UserReview
+          key={review.id}
+          review={review}
+        />)
+      )}
+    </ul>
+  );
+};
 
 export default ReviewsList;

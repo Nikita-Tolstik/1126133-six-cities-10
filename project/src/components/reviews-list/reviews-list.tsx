cc2141275db1c17a541c5c033526c783a2b24b98
Nikita Tolstik <tolstik-nik@mail.ru>
2022-08-12 16:53:25 +0300

    Переименовывает переменные в ReviewsList

--- project/src/components/reviews-list/reviews-list.tsx
+++ project/src/components/reviews-list/reviews-list.tsx
@@ -9,12 +9,12 @@ type ReviewsListProps = {
 const MAX_COMMENT = 10;
 
 const ReviewsList: React.FC<ReviewsListProps> = ({ reviews }) => {
-  const orderedReviews = reviews.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
-  const limitReviews = orderedReviews.length > MAX_COMMENT ? orderedReviews.slice(0, MAX_COMMENT) : orderedReviews;
+  const sortedReviews = reviews.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
+  const limitedReviews = sortedReviews.length > MAX_COMMENT ? sortedReviews.slice(0, MAX_COMMENT) : sortedReviews;
 
   return (
     <ul className="reviews__list">
-      {limitReviews.map((review) => (
+      {limitedReviews.map((review) => (
         <UserReview
           key={review.id}
           review={review}

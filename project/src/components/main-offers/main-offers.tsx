a5b60356ecedd886f7749b70164b3e12483112a1
Nikita Tolstik <tolstik-nik@mail.ru>
2022-08-12 17:45:58 +0300

    Подключает SortOptions в MainOffers

--- project/src/components/main-offers/main-offers.tsx
+++ project/src/components/main-offers/main-offers.tsx
@@ -1,8 +1,9 @@
 import React, { useCallback, useState } from 'react';
-import OffersList from '../offers-list/offers-list';
-import Map from '../map/map';
 import { Offers } from '../../types/offers';
 import { City, MapClass, PageCardClass } from '../../const';
+import Map from '../map/map';
+import OffersList from '../offers-list/offers-list';
+import SortOptions from '../sort-options/sort-options';
 
 
 type MainOffersProps = {
@@ -31,21 +32,7 @@ const MainOffers: React.FC<MainOffersProps> = (props) => {
           {offersCount} places to stay in Amsterdam
         </b>
 
-        <form className="places__sorting" action="#" method="get">
-          <span className="places__sorting-caption">Sort by&nbsp;</span>
-          <span className="places__sorting-type" tabIndex={0}>
-            Popular
-            <svg className="places__sorting-arrow" width="7" height="4">
-              <use xlinkHref="#icon-arrow-select"></use>
-            </svg>
-          </span>
-          <ul className="places__options places__options--custom">
-            <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-            <li className="places__option" tabIndex={0}>Price: low to high</li>
-            <li className="places__option" tabIndex={0}>Price: high to low</li>
-            <li className="places__option" tabIndex={0}>Top rated first</li>
-          </ul>
-        </form>
+        <SortOptions />
 
         <div className="cities__places-list places__list tabs__content">
           <OffersList

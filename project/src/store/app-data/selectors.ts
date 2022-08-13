b547ec3f35f710950d05de29b418ecd6b03ab33a
Nikita Tolstik <tolstik-nik@mail.ru>
2022-08-14 00:20:25 +0300

    Добавляет в selectors AppData selector для одного предложения

--- project/src/store/app-data/selectors.ts
+++ project/src/store/app-data/selectors.ts
@@ -1,19 +1,24 @@
 import { NameSpace } from '../../const';
-import { Offers } from '../../types/offers';
+import { Offer, Offers } from '../../types/offers';
 import { State } from '../../types/state';
 import { createSelector } from '@reduxjs/toolkit';
 import { getActiveCity } from '../app-process/selectors';
 import { getActiveCityOffers } from '../../utils/utils';
 
-export const getOffers = (state: State): Offers => state[NameSpace.Data].offers;
-export const getDataLoadedStatus = (state: State): boolean => state[NameSpace.Data].isDataLoaded;
+export const getOffersList = (state: State): Offers => state[NameSpace.Data].offersList;
+
+export const getOffersListLoadStatus = (state: State): boolean => state[NameSpace.Data].isOffersListLoading;
+
+export const getOffer = (state: State): Offer | null => state[NameSpace.Data].offer;
+
+export const getOfferLoadStatus = (state: State): boolean => state[NameSpace.Data].isOfferLoading;
 
 export const filterActiveCityOffers = createSelector(
-  [getActiveCity, getOffers],
-  (activeCity, offers) => getActiveCityOffers(activeCity, offers)
+  [getActiveCity, getOffersList],
+  (activeCity, offersList) => getActiveCityOffers(activeCity, offersList)
 );
 
 export const getIsEmptyOffers = createSelector(
-  [getOffers],
-  (offers) => !offers.length
+  [getOffersList],
+  (offersList) => !offersList.length
 );

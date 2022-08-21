08f6f017af06b5ec46c0deed443e30f190ac3362
Nikita Tolstik <tolstik-nik@mail.ru>
2022-08-22 02:23:42 +0300

    Реализует в FavoriteButton функцию добавления/удаления понравившегося объявления в избранное

--- project/src/components/favorite-button/favorite-button.tsx
+++ project/src/components/favorite-button/favorite-button.tsx
@@ -1,27 +1,95 @@
-import React from 'react';
+import React, { MouseEvent, useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
-import { ComponentClass, ButtonSize } from '../../const';
+import { ComponentClass, ButtonSize, AuthorizationStatus, AppRoute, FavoriteAction, FavoriteActionInfo, RequestStatus, Timer, TOAST_TYPE } from '../../const';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { getAuthorizationStatus } from '../../store/user-process/selectors';
+import { redirectToRoute } from '../../store/action';
+import { postUserFavoriteAction } from '../../store/api-actions';
+import { toast } from 'react-toastify';
 
 type FavoriteButtonProps = {
+  offerId: number;
+  favoriteStatus: boolean;
   buttonClass: ComponentClass,
-  isFavorite: boolean;
 }
 
 
-const FavoriteButton: React.FC<FavoriteButtonProps> = ({ buttonClass, isFavorite }) => {
+const FavoriteButton: React.FC<FavoriteButtonProps> = ({ offerId, favoriteStatus, buttonClass }) => {
+  const [isFavorite, setIsFavorite] = useState<boolean>(favoriteStatus);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const dispatch = useAppDispatch();
+
+  const authorizationStatus = useAppSelector(getAuthorizationStatus);
+  const isNoAuthorized = authorizationStatus === AuthorizationStatus.NoAuth;
+
+  const actionType = isFavorite ? FavoriteAction.Delete : FavoriteAction.Add;
+  const actionTypeInfo = isFavorite ? FavoriteActionInfo.Removed : FavoriteActionInfo.Add;
   const buttonSize = buttonClass === ComponentClass.OfferCard ? ButtonSize.Small : ButtonSize.Big;
 
   const btnClass = classNames(`${buttonClass}__bookmark-button button`, {
     [`${buttonClass}__bookmark-button--active`]: isFavorite
   });
 
+  const toastId = useRef<number | string | null>(null);
+  const timerId = useRef<NodeJS.Timeout | undefined>(undefined);
+
+  const handleClick = async (evt: MouseEvent<HTMLButtonElement>) => {
+
+    if (isNoAuthorized) {
+      dispatch(redirectToRoute(AppRoute.Login));
+      return;
+    }
+
+    toast.dismiss();
+    setIsLoading(true);
+
+    toastId.current = toast.loading(FavoriteActionInfo.Loading);
+
+    const responseData = await dispatch(postUserFavoriteAction({
+      id: offerId,
+      status: actionType
+    }));
+
+    if (responseData.meta.requestStatus === RequestStatus.Fulfilled) {
+      setIsFavorite((prevState) => !prevState);
+      toast.update(toastId.current, {
+        render: actionTypeInfo,
+        type: TOAST_TYPE,
+        isLoading: false,
+        autoClose: Timer.ToastClose
+      });
+    } else {
+      toast.dismiss(toastId.current);
+    }
+
+    timerId.current = setTimeout(() => {
+      setIsLoading(false);
+      timerId.current = undefined;
+    }, Timer.Favorite);
+  };
+
+  useEffect(
+    () =>
+      () => {
+        if (toastId.current && toast.isActive(toastId.current)) {
+          toast.dismiss(toastId.current);
+        }
+        if (timerId.current) {
+          clearTimeout(timerId.current);
+        }
+      }, []);
+
+
   return (
     <button
+      onClick={handleClick}
       className={btnClass}
       type="button"
+      disabled={isLoading}
     >
       <svg
-        className="place-card__bookmark-icon"
+        className={`${buttonClass}__bookmark-icon`}
         width={buttonSize.width}
         height={buttonSize.height}
       >
@@ -33,3 +101,4 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({ buttonClass, isFavorite
 };
 
 export default FavoriteButton;
+

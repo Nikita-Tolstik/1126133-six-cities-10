774bfefce01540c4cf37ceb2da7149184bdb0c7c
Nikita Tolstik <tolstik-nik@mail.ru>
2022-08-24 17:37:05 +0300

    Блокирует ссылку на страницу favorite, если уже на странице favorite

--- project/src/components/header-user/header-user.tsx
+++ project/src/components/header-user/header-user.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import React, { Fragment, MouseEvent, useRef, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { AppRoute, LogoutText, Timer } from '../../const';
 import { useAppDispatch, useAppSelector } from '../../hooks';
@@ -10,41 +10,47 @@ import { getLogoutErrorStatus, getLogoutProcessStatus, getUserEmail } from '../.
 import { clearLogoutError } from '../../store/user-process/user-process';
 
 const HeaderUser: React.FC = () => {
-  const timerRef = useRef<NodeJS.Timeout | undefined>(undefined);
-
   const dispatch = useAppDispatch();
+  const currentPath = useLocation().pathname;
+  const timerIdRef = useRef<NodeJS.Timeout | undefined>(undefined);
+
   const favoriteList = useAppSelector(getFavoriteList);
   const userEmail = useAppSelector(getUserEmail);
 
   const isFavoriteLoading = useAppSelector(getFavoriteLoadStatus);
   const isLogoutProcessing = useAppSelector(getLogoutProcessStatus);
   const isLogoutError = useAppSelector(getLogoutErrorStatus);
+  const isFavoritePath = currentPath === AppRoute.Favorite;
 
   const favoriteCount = isFavoriteLoading ? '' : favoriteList.length;
   const signoutText = isLogoutProcessing ? LogoutText.Exiting : LogoutText.SignOut;
 
-  const signoutClass = classNames('header__signout', {
-    'header__signout__error horizontal-shake': isLogoutError
+  const linkUserNameClass = classNames('header__nav-link header__nav-link--profile', {
+    'link__disabled': isFavoritePath
   });
 
-  const linkClass = classNames('header__nav-link', {
+  const linkSingoutClass = classNames('header__nav-link', {
     'link__disabled': isLogoutError || isLogoutProcessing
   });
 
-  if (isLogoutError && !timerRef.current) {
-    timerRef.current = setTimeout(() => {
+  const spanSignoutClass = classNames('header__signout', {
+    'header__signout__error horizontal-shake': isLogoutError
+  });
+
+  if (isLogoutError && !timerIdRef.current) {
+    timerIdRef.current = setTimeout(() => {
       dispatch(clearLogoutError());
-      timerRef.current = undefined;
+      timerIdRef.current = undefined;
     }, Timer.Logout);
   }
 
   useEffect(
     () =>
       () => {
-        if (timerRef.current) {
+        if (timerIdRef.current) {
           toast.dismiss();
           dispatch(clearLogoutError());
-          clearTimeout(timerRef.current);
+          clearTimeout(timerIdRef.current);
         }
       }, [dispatch]);
 
@@ -54,7 +60,7 @@ const HeaderUser: React.FC = () => {
       <li className="header__nav-item user">
         <Link
           to={AppRoute.Favorite}
-          className="header__nav-link header__nav-link--profile"
+          className={linkUserNameClass}
         >
           <div className="header__avatar-wrapper user__avatar-wrapper" />
 
@@ -74,9 +80,9 @@ const HeaderUser: React.FC = () => {
               dispatch(logoutAction());
             }
           }}
-          className={linkClass} href="/"
+          className={linkSingoutClass} href="/"
         >
-          <span className={signoutClass}>{signoutText}</span>
+          <span className={spanSignoutClass}>{signoutText}</span>
         </a>
       </li>
     </Fragment>
